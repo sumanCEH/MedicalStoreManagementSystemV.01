e3b90af766ec5a00674f32e3d206d7428fe35065
sumanCEH <sumansarkarcob043@gmail.com>
2022-12-13 19:51:08 +0530

    add user fname,lname,ph,address

--- src/main/java/com/capgemini/entity/User.java
+++ src/main/java/com/capgemini/entity/User.java
@@ -1,46 +1,100 @@
 package com.capgemini.entity;
 
+
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name="user_table")
 public class User {
-	
-	@Id
+    
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
-	private String email;
-	private String password;
-	private String role;
-	
-	public int getUserId() {
-		return userId;
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private String email;
+    private String password;
+    private String address;
+    private String role;
+    
+    
+    
+    public User() {
+		
 	}
-	public void setUserId(int userId) {
+	public User(int userId, String firstName, String lastName, String phoneNo, String email, String password,
+			String address, String role) {
+		super();
 		this.userId = userId;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNo = phoneNo;
 		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
 		this.password = password;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
+		this.address = address;
 		this.role = role;
 	}
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", email=" + email + ", password=" + password + ", role=" + role + "]";
-	}
-
+	public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNo=" + phoneNo
+                + ", email=" + email + ", password=" + password + ", address=" + address + ", role=" + role + "]";
+    }
+    
+    
 }
