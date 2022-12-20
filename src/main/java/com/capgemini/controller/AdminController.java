a53e48c5e1955303ccde5e5e360bdd1bc3a88e0f
uditya-patel <66989814+uditya-patel@users.noreply.github.com>
2022-12-21 00:06:27 +0530

    uncommented import

--- src/main/java/com/capgemini/controller/AdminController.java
+++ src/main/java/com/capgemini/controller/AdminController.java
@@ -1,6 +1,6 @@
 package com.capgemini.controller;
 
-//import java.util.List;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
