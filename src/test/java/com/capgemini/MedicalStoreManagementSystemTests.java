37e2b414fa449069f98d1402003dab17c661edd9
uditya-patel <udityap@gmail.com>
2022-12-21 14:05:00 +0530

    some change

--- src/test/java/com/capgemini/MedicalStoreManagementSystemTests.java
+++ src/test/java/com/capgemini/MedicalStoreManagementSystemTests.java
@@ -1,10 +1,10 @@
-package com.bootcamp.bookstoremanagement;
+package com.capgemini;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookstoreManagementApplicationTests {
+class MedicalStoreManagementSystemTests {
 
 	@Test
 	void contextLoads() {
